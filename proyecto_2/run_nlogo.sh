af35cc302cb03afd38280c7b6871c1f1125be359
Alfonso Kim <alfonsokim84@gmail.com>
2015-05-25 21:25:17 -0500

    comentarios al xml y el bash

--- proyecto_2/run_nlogo.sh
+++ proyecto_2/run_nlogo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Cambiar el classpath a su instalacion de netlogo
 export CLASSPATH=/Applications/NetLogo\ 5.1.0/NetLogo.jar:.
 
 rm -f cancer.csv
@@ -8,4 +9,5 @@ HEADER="enable-growth,enable-replicative-cap,enable-apoptosis,enable-metastasis,
 
 echo $HEADER > cancer.csv
 
+# Darle mas RAM para que corra mas rapido
 java -Xmx1024m -Dfile.encoding=UTF-8 org.nlogo.headless.Main --model proyecto.nlogo --setup-file experiments.xml
